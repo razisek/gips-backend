664b894d9f872c920e3549db2e8c87a44b5cb1de
razisek <agusdono27@gmail.com>
2023-06-16 09:49:52 +0700

    update verify jwt token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
+const db = require("../models")
+const User = db.user;
 
 verifyToken = (req, res, next) => {
     let token = req.headers["x-access-token"];
@@ -8,10 +10,16 @@ verifyToken = (req, res, next) => {
         return res.status(401).send({ message: "Unauthorized!" });
     }
 
-    jwt.verify(token, config.secret, (err, decoded) => {
+    jwt.verify(token, config.secret, async (err, decoded) => {
         if (err) {
             return res.status(401).send({ message: "Unauthorized!" });
         }
+
+        const profile = await User.findById(decoded.id).exec();
+        if (!profile) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        
         req.userId = decoded.id;
         next();
     });
